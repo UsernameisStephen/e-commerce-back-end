2d9482f347663f6a72b4199df2716b8d47a6ac19
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-10 14:34:11 -0500

    added delete function to the product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const delProduct = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+   
+    if (!delProduct) {
+      res.status(404).json({message: 'No such Product ID was found!'});
+      return;
+    }
+    res.status(200).json(delProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 module.exports = router;
