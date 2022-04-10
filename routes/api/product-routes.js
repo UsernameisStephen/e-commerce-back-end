fac93b6d0b1b7973f65e552a95e304dc509449f2
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-10 14:13:25 -0500

    created get route for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const getProducts = await Product.findAll({
+      include: [Category, Tag]
+    });
+    res.status(200).json(getProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const getProducts = await Product.findByPk(req.params.id, {include: [Category, Tag] });
+      if (!getProducts) {
+        res.status(404).json({message: 'This is not the Product ID you are looking for.'});
+      return;
+    }
+    res.status(200).json(getProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // create new product
