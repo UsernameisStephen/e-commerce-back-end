1c1be35af7734c6c0e8c96ec668783753506019d
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-10 15:18:15 -0500

    added post/put to cat route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,12 +30,35 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const addCat = await Category.create({
+      category_name: req.body.category_name,
+    });
+   
+    res.status(200).json(addCat);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+
+    try {
+      const updateCat = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+      category_name: req.body.category_name
+    })
+    res.status(200).json(updateCat);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  
 });
 
 router.delete('/:id', (req, res) => {
