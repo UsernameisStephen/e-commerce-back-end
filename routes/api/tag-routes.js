bbb764effbdd5eab193f677987e2720b39547534
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-10 16:35:51 -0500

    added delete router to tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,6 +61,21 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+   
+    if (!deleteTag) {
+      res.status(404).json({message: 'This is not the Tag you were looking for.'});
+      return;
+    }
+    res.status(200).json(deleteTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
