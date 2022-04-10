d46092e16d8a89e05484258ef667d6808268ec4d
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-10 15:05:49 -0500

    added get for cat route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,31 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const getCategories = await Category.findAll ({include: [{ model: Product }], });
+    res.status(200).json(getCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const getCategories = await Category.findByProd(req.params.id, {include: [Product] });
+    if (!getCategories) {
+      res.status(404).json({message: 'This is not the Product ID you were looking for.'});
+      return;
+    }
+    res.status(200).json(getCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
