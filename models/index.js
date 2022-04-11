f27ca1514115bad8d89401e9c80eb4bced7c0899
Stephen Elliott <stephenmatthewelliott@gmail.com>
2022-04-10 23:21:50 -0500

    added many to many association

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
-// Categories have many Products
+
+// Categories have many Products  
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+
 
 // Products belongToMany Tags (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
 
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
 
 module.exports = {
   Product,
